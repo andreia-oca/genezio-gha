9ea819d6d8855372a38867714f49090b8bc6df46
Andreia Ocănoaia <andreia.ocanoaia@gmail.com>
2024-10-31 14:31:20 +0200

    Add a GHA to deploy with a custom secret
    Signed-off-by: Andreia Ocănoaia <andreia.ocanoaia@gmail.com>

--- index.mjs
+++ index.mjs
@@ -1,7 +1,10 @@
 export const handler = async (event) => {
     console.log("Function was called");
-    console.log(event);
     const name = event.queryStringParameters?.name || "World";
+    console.log(`Name: ${name}`);
+
+    console.log(process.env["MY_SECRET"]);
+
     return {
       statusCode: 200,
       body: `Hello, ${name}! Welcome to Genezio Functions!`,
